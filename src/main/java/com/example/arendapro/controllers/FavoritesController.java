ab8ebe793db55928df499fb7ae0015afe2d2356b
Namdar1Ibrakhim <namdar_ibrahim@mail.ru>
2023-10-28 02:35:16 +0500

    update

--- src/main/java/com/example/arendapro/controllers/FavoritesController.java
+++ src/main/java/com/example/arendapro/controllers/FavoritesController.java
@@ -2,13 +2,13 @@ package com.example.arendapro.controllers;
 
 import com.example.arendapro.dto.FavoritesRequestDto;
 import com.example.arendapro.dto.FavoritesResponseDto;
+import com.example.arendapro.security.user.User;
 import com.example.arendapro.service.FavoritesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +22,14 @@ public class FavoritesController {
     @GetMapping()
     public ResponseEntity<List<FavoritesResponseDto>> getAllMyFavorites(){
         return ResponseEntity.ok(favoritesService.getAllMyFavorites());
+    }
+    @RequestMapping("/add/{immovable_id}")
+    public ResponseEntity<FavoritesResponseDto> addFavorites(@PathVariable Integer immovable_id, @AuthenticationPrincipal User user){
+        return ResponseEntity.ok(favoritesService.addFavorites(immovable_id, user));
+    }
+    @DeleteMapping("/delete/favorites_id")
+    public ResponseEntity deleteFavorites(@PathVariable Integer favorites_id){
+        favoritesService.deleteFavorites(favorites_id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/arendapro/mapper/FavoritesMapper.java
+++ src/main/java/com/example/arendapro/mapper/FavoritesMapper.java
@@ -3,7 +3,9 @@ package com.example.arendapro.mapper;
 import com.example.arendapro.dto.FavoritesRequestDto;
 import com.example.arendapro.dto.FavoritesResponseDto;
 import com.example.arendapro.entity.Favorites;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface FavoritesMapper{
     FavoritesResponseDto toDto(Favorites favorites);
     Favorites toEntity(FavoritesRequestDto favoritesRequestDto);
