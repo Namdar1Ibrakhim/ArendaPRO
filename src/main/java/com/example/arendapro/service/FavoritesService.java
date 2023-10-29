39101c5fa1172a7a7e9427938d1c70a518b90cbb
Namdar1Ibrakhim <namdar_ibrahim@mail.ru>
2023-10-29 20:16:35 +0500

    update

--- src/main/java/com/example/arendapro/service/FavoritesService.java
+++ src/main/java/com/example/arendapro/service/FavoritesService.java
@@ -1,13 +1,12 @@
 package com.example.arendapro.service;
 
-import com.example.arendapro.dto.FavoritesResponseDto;
-import com.example.arendapro.entity.Favorites;
+import com.example.arendapro.dto.FavoritesDto;
 import com.example.arendapro.security.user.User;
 
 import java.util.List;
 
 public interface FavoritesService {
-    List<FavoritesResponseDto> getAllMyFavorites();
-    FavoritesResponseDto addFavorites(Integer immovable_id, User user);
+    List<FavoritesDto> getAllMyFavorites();
+    FavoritesDto addFavorites(Integer immovable_id, User user);
     void deleteFavorites(Integer favorites_id);
 }
