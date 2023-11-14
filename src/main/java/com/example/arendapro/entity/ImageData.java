ed082170d33a5dc53dcc5f872d0e617c45c444ae
Namdar1Ibrakhim <namdar_ibrahim@mail.ru>
2023-11-14 17:56:58 +0500

    update

---
+++ src/main/java/com/example/arendapro/entity/ImageData.java
@@ -0,0 +1,31 @@
+package com.example.arendapro.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageData {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    @Column(name = "imagedata", length = 1000)
+    private byte[] imageData;
+
+    @OneToOne
+    private Immovables immovables;
+
+}
