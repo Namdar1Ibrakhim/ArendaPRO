72018df860917df3a98bd6bc9cfff207f3c85a8e
Namdar1Ibrakhim <namdar_ibrahim@mail.ru>
2023-11-20 12:51:21 +0500

    update

--- src/main/java/com/example/arendapro/service/StorageService.java
+++ src/main/java/com/example/arendapro/service/StorageService.java
@@ -1,11 +1,16 @@
 package com.example.arendapro.service;
 
+import com.example.arendapro.entity.Immovables;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface StorageService {
 
-    String uploadImage(MultipartFile file) throws IOException;
+    String uploadImage(MultipartFile file, Integer immovable_id) throws IOException;
     byte[] downloadImage(Integer id);
+    List<Integer> getImagesByImmovable(Integer immovable_id);
+    String uploadImageList(MultipartFile[] file, Integer immovable_id) throws IOException;
+    void deleteImageById(Integer image_id);
 }
--- src/main/java/com/example/arendapro/service/impl/StorageServiceImpl.java
+++ src/main/java/com/example/arendapro/service/impl/StorageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.arendapro.service.impl;
 
 import com.example.arendapro.entity.ImageData;
+import com.example.arendapro.entity.Immovables;
+import com.example.arendapro.repository.ImmovablesRepository;
 import com.example.arendapro.repository.StorageRepository;
 import com.example.arendapro.service.StorageService;
 import com.example.arendapro.util.ImageUtils;
@@ -9,28 +11,78 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StorageServiceImpl implements StorageService {
 
-    private final StorageRepository repository;
+    private final StorageRepository storageRepository;
+    private final ImmovablesRepository immovablesRepository;
 
     @Override
-    public String uploadImage(MultipartFile file) throws IOException {
-        ImageData imageData = repository.save(ImageData.builder()
+    public String uploadImage(MultipartFile file, Integer immovable_id) throws IOException {
+        ImageData imageData = storageRepository.save(ImageData.builder()
                     .name(file.getOriginalFilename())
                     .type(file.getContentType())
+                    .immovable(immovablesRepository.findById(immovable_id).get())
                     .imageData(ImageUtils.compressImage(file.getBytes())).build());
         if(imageData!=null){
             return "file uploaded successfully: " + file.getOriginalFilename();
         }
         return null;
     }
+
+    @Override
+    public String uploadImageList(MultipartFile[] files, Integer immovable_id) throws IOException {
+        for (MultipartFile file : files) {
+             storageRepository.save(ImageData.builder()
+                    .name(file.getOriginalFilename())
+                    .type(file.getContentType())
+                     .immovable(immovablesRepository.findById(immovable_id).get())
+                    .imageData(ImageUtils.compressImage(file.getBytes())).build());
+        }
+        return "files uploaded successfully";
+
+    }
+
+    @Override
+    public void deleteImageById(Integer image_id) {
+        storageRepository.delete(storageRepository.findById(image_id).get());
+    }
+
     public byte[] downloadImage(Integer id){
-        Optional<ImageData> dbImageData = repository.findById(id);
+        Optional<ImageData> dbImageData = storageRepository.findById(id);
         byte[] images = ImageUtils.decompressImage(dbImageData.get().getImageData());
         return images;
     }
+
+    @Override
+    public List<Integer> getImagesByImmovable(Integer immovable_id) {
+        List<ImageData> dbImageDataList = storageRepository.findImageDataByImmovable_id(immovable_id);
+        List<Integer> listOfId = dbImageDataList.stream()
+                .map(ImageData::getId)
+                .collect(Collectors.toList());
+        return listOfId;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
