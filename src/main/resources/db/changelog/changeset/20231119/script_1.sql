cfb408359023873ede6758b69de8aed882be87e8
Namdar1Ibrakhim <namdar_ibrahim@mail.ru>
2023-11-20 12:51:08 +0500

    update

---
+++ src/main/resources/db/changelog/changeset/20231119/script_1.sql
@@ -0,0 +1,14 @@
+insert into country
+values(1, 'KZ', 'Kazakhstan');
+
+insert into region
+values(1, 'Almaty');
+
+insert into city
+values(1, 'Almaty');
+
+insert into district
+values(1, 'Ayezovski');
+
+insert into street
+values(1, 'Zhetisy');
