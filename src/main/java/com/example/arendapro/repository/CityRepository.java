2eaee498beb2e78fe079566e95b297ad34ae3afd
Namdar1Ibrakhim <namdar_ibrahim@mail.ru>
2023-11-21 22:02:51 +0500

    update

--- src/main/java/com/example/arendapro/repository/CityRepository.java
+++ src/main/java/com/example/arendapro/repository/CityRepository.java
@@ -2,6 +2,8 @@ package com.example.arendapro.repository;
 
 import com.example.arendapro.entity.address.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +15,10 @@ public interface CityRepository extends JpaRepository<City, Integer> {
     List<City> findAll();
 
     Optional<City> findById(Integer id);
+
+    @Modifying
+    @Query("INSERT INTO city values(:name)")
+    void setCity(String name);
 
 
 }
