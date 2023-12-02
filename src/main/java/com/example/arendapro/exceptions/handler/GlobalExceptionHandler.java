84b1926e745e70885f8ba27d4865d33e290dc019
Namdar1Ibrakhim <namdar_ibrahim@mail.ru>
2023-12-02 15:56:35 +0500

    update

--- src/main/java/com/example/arendapro/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/arendapro/exceptions/handler/GlobalExceptionHandler.java
@@ -5,17 +5,50 @@ import com.example.arendapro.enums.ErrorCode;
 import com.example.arendapro.exceptions.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ImageUploadException.class)
+    public ResponseEntity<ErrorResponse> exceptionImageUploadHandler(ImageUploadException e){
+        log.info(e.getMessage());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(e.getMessage())
+                .errorCode(404)
+                .build();
+        return ResponseEntity
+                .badRequest()
+                .body(errorResponse);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> exceptionInvalidArgumentHandler(MethodArgumentNotValidException ex){
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return errorMap;
+
+
+    }
 
     @ExceptionHandler(UserAlreadyExistAuthenticationException.class)
     public ResponseEntity<ErrorResponse> exceptionUserAlreadyExistHandler(UserAlreadyExistAuthenticationException e){
