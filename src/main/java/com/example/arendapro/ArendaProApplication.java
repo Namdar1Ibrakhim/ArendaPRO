4638cf5647324971a03f626be4c256c0d8eca476
Namdar1Ibrakhim <namdar_ibrahim@mail.ru>
2023-12-02 15:55:46 +0500

    update

--- src/main/java/com/example/arendapro/ArendaProApplication.java
+++ src/main/java/com/example/arendapro/ArendaProApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Library APIS", version = "1.0", description = "Library Management Apis"))
+@EnableCaching
 public class ArendaProApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/arendapro/security/config/ApplicationConfig.java
+++ src/main/java/com/example/arendapro/security/config/ApplicationConfig.java
@@ -1,11 +1,20 @@
 package com.example.arendapro.security.config;
 
+import com.example.arendapro.entity.User;
 import com.example.arendapro.repository.UserRepository;
+import com.example.arendapro.util.MinioProperties;
+import io.minio.MinioClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -23,13 +32,13 @@ public class ApplicationConfig {
 
 
     private final UserRepository repository;
+    private final MinioProperties minioProperties;
 
     @Bean
     public UserDetailsService userDetailsService(){
         return username -> repository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
-
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -45,6 +54,15 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getUrl())
+                .credentials(minioProperties.getAccessKey(),
+                        minioProperties.getSecretKey())
+                .build();
     }
 }
 
